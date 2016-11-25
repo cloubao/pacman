27066e8b7b7d9773ecf01818f582cfa0cbba2095
Charles-Eugene Loubao <charlesloubao95@gmail.com>
2016-11-25 13:10:22 -0500

    Cleanup

--- DependencyInjection/Injector.cs
+++ DependencyInjection/Injector.cs
@@ -9,6 +9,7 @@ namespace DependencyInjection
 {
     // Dependency injection mechanism inspired by Dagger2 (Dependency Injection for Android/Java)
     // The injector initialize the dependencies of an object by using the module as a provider
+    // TODO: Use delegate methods instead of injector objects for the injection
     public interface Injector<M, T> where M: IModule
     {
         void inject(M module, T mObject); 
